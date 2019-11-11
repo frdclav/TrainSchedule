d91ad76ba40ef715cc0e7ce9a838f580f6dd6dcc
Francis Rodson Clavel <frdclav@gmail.com>
2019-11-11 12:54:33 -0800

    cleans up app.js code

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const newTrainObj = function(name, dest, time, freq) {
     this.time = time;
     this.freq = freq;
 
-    // console.log(this);
 };
 
 // array of train objects 
@@ -24,68 +23,7 @@ let trainObjArr = [];
 
 // MOMENT.JS stuff =========================================================
 // =========================================================================
-// Assume the following situations.
 
-// (TEST 1)
-// First Train of the Day is 3:00 AM
-// Assume Train comes every 3 minutes.
-// Assume the current time is 3:16 AM....
-// What time would the next train be...? (Use your brain first)
-// It would be 3:18 -- 2 minutes away
-
-// (TEST 2)
-// First Train of the Day is 3:00 AM
-// Assume Train comes every 7 minutes.
-// Assume the current time is 3:16 AM....
-// What time would the next train be...? (Use your brain first)
-// It would be 3:21 -- 5 minutes away
-
-
-// ==========================================================
-
-// Solved Mathematically
-// Test case 1:
-// 16 - 00 = 16
-// 16 % 3 = 1 (Modulus is the remainder)
-// 3 - 1 = 2 minutes away
-// 2 + 3:16 = 3:18
-
-// Solved Mathematically
-// Test case 2:
-// 16 - 00 = 16
-// 16 % 7 = 2 (Modulus is the remainder)
-// 7 - 2 = 5 minutes away
-// 5 + 3:16 = 3:21
-
-// // Assumptions
-// var tFrequency = 3;
-
-// // Time is 3:30 AM
-// var firstTime = "03:30";
-
-// // First Time (pushed back 1 year to make sure it comes before current time)
-// var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-// console.log(firstTimeConverted);
-
-// // Current Time
-// var currentTime = moment();
-// console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-// // Difference between the times
-// var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-// console.log("DIFFERENCE IN TIME: " + diffTime);
-
-// // Time apart (remainder)
-// var tRemainder = diffTime % tFrequency;
-// console.log(tRemainder);
-
-// // Minute Until Train
-// var tMinutesTillTrain = tFrequency - tRemainder;
-// console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-// // Next Train
-// var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-// console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
 
 // calculate next Arrival
@@ -98,32 +36,26 @@ const trainArrival = function(firstTime, freq) {
 
 };
 const calcNextArrival = (time, freq) => {
-        var currentTime = moment();
-        // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-        var firstTime = time;
-        // First Time (pushed back 1 year to make sure it comes before current time)
-        var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-        // console.log(firstTimeConverted);
+    var currentTime = moment();
+    var firstTime = time;
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
 
-        // Difference between the times
-        var diffTime = currentTime.diff(moment(firstTimeConverted), "minutes");
-        // console.log("DIFFERENCE IN TIME: " + diffTime);
-        // console.log('freq', freq)
-        // Time apart (remainder)
-        var tRemainder = diffTime % parseInt(freq);
-        // console.log(tRemainder);
+    // Difference between the times
+    var diffTime = currentTime.diff(moment(firstTimeConverted), "minutes");
 
-        // Minute Until Train
-        var tMinutesTillTrain = parseInt(freq) - tRemainder;
-        // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    // Time apart (remainder)
+    var tRemainder = diffTime % parseInt(freq);
 
-        // Next Train
-        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-        // console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
-        return [tMinutesTillTrain, moment(nextTrain).format("HH:mm")]
-    }
-    // MOMENT.JS stuff =========================================================
-    // =========================================================================
+    // Minute Until Train
+    var tMinutesTillTrain = parseInt(freq) - tRemainder;
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    return [tMinutesTillTrain, moment(nextTrain).format("HH:mm")]
+};
+// MOMENT.JS stuff =========================================================
+// =========================================================================
 
 
 //  function to showNewTrain on table
@@ -160,7 +92,6 @@ const showNewTrain = (train) => {
 
         var nextArr = '#' + random_id + '-next-arrival';
         var minsAway = '#' + random_id + '-mins-away';
-        // console.log(calcNextArrival($(nextArr).attr('data-first-train-time'), parseInt($(nextArr).attr('data-freq')))[1], calcNextArrival($(minsAway).attr('data-first-train-time'), parseInt($(minsAway).attr('data-freq')))[0])
         $(nextArr).text(calcNextArrival($(nextArr).attr('data-first-train-time'), parseInt($(nextArr).attr('data-freq')))[1]);
         $(minsAway).text(calcNextArrival($(minsAway).attr('data-first-train-time'), parseInt($(minsAway).attr('data-freq')))[0]);
 
@@ -197,7 +128,7 @@ var ref = database.ref();
 
 // function to push stuff into db
 const pushNewTrain = obj => {
-    console.log("We're adding a new train to the database! ->", obj)
+    // console.log("We're adding a new train to the database! ->", obj)
     ref.push({
         trainName: obj.name,
         trainDest: obj.dest,
@@ -210,7 +141,7 @@ const pushNewTrain = obj => {
 ref.on('child_added', function(snapshot) {
 
         var snap = snapshot.val()
-        console.log("There's a new train on the database!", snap)
+            // console.log("There's a new train on the database!", snap)
         var newTrain = new newTrainObj(snap.trainName, snap.trainDest, snap.trainTime, snap.trainFreq);
         showNewTrain(newTrain);
     },
@@ -226,10 +157,10 @@ ref.on('child_added', function(snapshot) {
 newTrainSubmitBtn.on('click', function(event) {
     event.preventDefault();
     if ((/^([01]\d|2[0-3]):?([0-5]\d)$/.test(newTrainTime.val()) && newTrainTime.val() <= 2301) && (/^[0-9]*$/.test(newTrainFreq.val())) && (newTrainName.val() !== '') && (newTrainDest.val() !== '')) {
-        console.log('it matches!')
+        // console.log('it matches!')
         let newTrain = new newTrainObj(newTrainName.val(), newTrainDest.val(), newTrainTime.val(), newTrainFreq.val());
         trainObjArr.push(newTrain);
-        console.log('currently:', trainObjArr)
+        // console.log('currently:', trainObjArr)
         pushNewTrain(newTrain);
     } else {
         alert('Please make sure that all fields are correctly filled out.')
@@ -239,10 +170,6 @@ newTrainSubmitBtn.on('click', function(event) {
 // display the current time
 
 setInterval(function() {
-    // const arrivalTime = new Date()
-    // const t = arrivalTime.toTimeString();
-    // const tShort = t
-    // .substring(0, 5);
-    // console.log(moment().format("HH:mm:ss"));
+
     $("#curTime").text(moment().format("HH:mm:ss"))
 }, 100);
